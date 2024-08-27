6a4273e1d1a63d9eecfbc4cfc8bd422641c3d966
Gabby Baniqued <gabrielle.baniqued@bishopireton.org>
2024-08-27 10:22:03 -0400

    New repo for new class

---
+++ README.md
@@ -0,0 +1,13 @@
+# How to use this repository
+
+There are 2 types of files you will be provided with in this class:
+(1) Starter code for projects
+(2) Starter code for in-class work
+
+In order to access these things, you will use git.
+
+First, clone this repository as shown in class.
+
+Every time I need you to access new files, you will use git pull to retrieve these files.
+
+You will not be using GitHub to share your files across your devices. You will use OneDrive.
---
+++ Unit 1/PrimitiveDataTypes.java
@@ -0,0 +1,36 @@
+public class PrimitiveDataTypes {
+
+   public static void main(String[] args) {
+      
+      // Declaring and initializing a variable in 2 steps
+           
+      // Declaring and initializing all at once
+      
+      // Storing the result of a mathematical expression/Java arithmetic
+      
+      // Representing Whole Numbers in Java
+       
+      // -128 to 127
+      
+      // -32,768 to 32,767
+      
+      // -2,147,483,648 to 2,147,483,647 (-2 billion to 2 billion)
+      
+      // -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 (Over 2 billion)
+      // -9 quintillion to 9 quintillion
+      
+      // Representing decimal values in Java
+      
+      // about 6 decimal digits
+      
+      // about 15 decimal digits
+      
+      // true or false
+      // boolean literal
+      // result of a boolean expression
+
+      // one character
+      
+   }
+
+}
