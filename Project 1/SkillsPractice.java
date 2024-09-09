9cab8866d84a83efa8966117ce78d3b256fdf42d
Gabby Baniqued <gabrielle.baniqued@bishopireton.org>
2024-09-09 12:36:57 -0400

    Create SkillsPractice.java

---
+++ Project 1/SkillsPractice.java
@@ -0,0 +1,125 @@
+/* Your header goes here.
+ * Project 1 Description: Outputs the results of various programming exercises.
+ */
+
+ public class SkillsPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Written By: ____________");
+
+        // method calls
+        // use comments to only run certain methods.
+
+        printTriangle(); // calls the printTriangle method
+
+        /* call the other methods below this line to 
+        test your code and produce the output*/
+        
+
+        
+    }
+
+
+    /* Without using iteration/loops, print an equilateral triangle of any size
+     * with asterisks (*) and spaces. Challenge: Use only 1 line of code. */
+    public static void printTriangle() {
+        // YOUR CODE FOR printSquare() GOES BELOW    
+
+    }
+
+    /* Using the provided variables, print out the following message:
+     * Jack has a 99.5% in Algebra 1!
+     */
+    public static void printGrade() {
+        double grade = 99.5;
+        String name = "Jack";
+        String course = "Algebra 1";
+
+        // YOUR CODE FOR printGrade() GOES BELOW
+    }
+
+    /* Using the declared variables, initialize each line using a haiku of your own creation.
+     * Then print each line on its own line using ONE LINE OF CODE ONLY.
+     * Requirement: the 1st and 3rd line must have 5 syllables, and the 2nd line must have 7 syllables.
+     * A traditional haiku is always about nature, but you may choose any topic.
+     * 
+     * EXAMPLE:
+     * On a withered branch
+     * A crow has alighted;
+     * Nightfall in autumn.
+     */
+    public static void haiku() {
+        String line1, line2, line3;
+
+        // YOUR CODE FOR haiku GOES BELOW HERE
+    }
+
+    /* Declare an initialize a variable that contains a quote of your choosing.
+     * Your quote MUST be surrounded by quotation marks (see escape characters on pg ____).
+     * Be sure to attribute your quote to its source.
+     * 
+     * EXAMPLE:
+     * “Be who you are and be that well. So that you may bring honor to the Master Craftsman whose handiwork you are.”
+     * - St. Francis De Sales
+     */
+    public static void favoriteQuote() {
+        // YOUR CODE FOR favoriteQuote GOES BELOW HERE
+    }
+
+    /* Using String method(s) extract the first and last characters 
+     * of word and output the concatenated result.
+     * You are NOT allowed to use charAt().
+     * 
+     * EXPECTED OUTPUT:
+     * Sr
+     */
+    public static void firstAndLast() {
+        String word = "September";
+        // YOUR CODE GOES BELOW HERE
+    }
+
+    /* Using String method(s) omit the first and last characters 
+     * of word and output the concatenated result.
+     * You are NOT allowed to use charAt().
+     * 
+     * EXPECTED OUTPUT:
+     * eptembe
+     */
+    public static void omitFirstAndLast() {
+        String word = "September";
+        // YOUR CODE GOES BELOW HERE
+    }
+
+    /* Concatenate the provided String variables below.
+     * Using String method(s), determine how many letters are in the final result
+     * and print out the following EXPECTED OUTPUT:
+     * 
+     * There are 11 characters in HelloWorld!
+    */
+    public static void howManyChars() {
+        String word1 = "Hello";
+        String word2 = "World!";
+        // YOUR CODE GOES BELOW HERE
+    }
+
+    /* Declare and initialize two String variables with identical String values.
+     * Use String method(s) to check that they are equal and print the result.
+     * 
+     * EXPECTED OUTPUT:
+     * Strings identical? true
+     */
+    public static void identicalStrings() {
+        // YOUR CODE GOES HERE
+    }
+
+    /* Declare and initialize two String variables with different String values.
+     * Use String method(s) to check that they are not equal and print the result.
+     * 
+     * EXPECTED OUTPUT:
+     * Strings identical? false
+     */
+    public static void differentStrings() {
+        // YOUR CODE GOES HERE
+    }
+
+}
