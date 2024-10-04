137d6bcef14f9c813279f506584282e2415c4ed6
Gabby Baniqued <gabrielle.baniqued@bishopireton.org>
2024-10-04 11:24:04 -0400

    Delete Lab 1

--- Unit 2/Lab1.java
+++
@@ -1,5 +0,0 @@
-      System.out.println("pigLatin(\"smile\") = " + pigLatin("smile")); // Expected output: "milesay"
-      
-      // Test swapAB method
-      System.out.println("swapAB(\"oops\", 1, 3) = " + swapAB("oops", 1, 3)); // Expected output: "ospo"
-      System.out.println("swapAB(\"hello\", 0, 4) = " + swapAB("hello", 0, 4)); // Expected output: "oellh"
