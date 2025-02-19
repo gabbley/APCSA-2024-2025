984cc70010c0fd4c159903592bd6cd29258b85ee
Gabby Baniqued <gabrielle.baniqued@bishopireton.org>
2025-02-19 11:16:05 -0500

    Add comments to Sorts.java

--- Unit 7/Sorts.java
+++ Unit 7/Sorts.java
@@ -1,33 +1,35 @@
 public class Sorts {
 
    public static void selectionSort(int[] elements) {
-      for (int j = 0; j < elements.length - 1; j++) {
-         int minIndex = j;
-         for (int k = j + 1; k < elements.length; k++) {
+      for (int j = 0; j < elements.length - 1; j++) { 
+         int minIndex = j; 
+         for (int k = j + 1; k < elements.length; k++) { // compares current "min" to rest of array
             if (elements[k] < elements[minIndex]) {
-               minIndex = k;
+               minIndex = k; // updates min if smaller value found
             }
          }
+         // after the inner loop, the unsorted min has been found, now needs to be swapped
          if (j != minIndex) {
-            int temp = elements[j];
-            elements[j] = elements[minIndex];
-            elements[minIndex] = temp;
+            int temp = elements[j]; // saves value to be overwritten as a temp variable
+            elements[j] = elements[minIndex]; // overwrites value
+            elements[minIndex] = temp; // re-inserts temp
          }
-      }
+      } // end of outer loop, process remaining elements
    }
    
    public static void insertionSort(int[] elements) {
-      for (int j = 1; j < elements.length; j++) {
+      for (int j = 1; j < elements.length; j++) { // start at 1 to always compare the left element
          int temp = elements[j];
          int possibleIndex = j;
-         while (possibleIndex > 0 && temp < elements[possibleIndex - 1]) {
-            elements[possibleIndex] = elements[possibleIndex - 1];
+         while (possibleIndex > 0 && temp < elements[possibleIndex - 1]) { // if the left element is greater than temp
+            elements[possibleIndex] = elements[possibleIndex - 1]; // shift elements until insertion point is reached
             possibleIndex--;
-         }
-         elements[possibleIndex] = temp;
+         } // while loop ends when proper insertion point is found
+         elements[possibleIndex] = temp; // re-inserts temp
       }
    }
    
+   // prints arr
    public static void print(int[] arr) {;
       for (int i : arr) {
          System.out.print(i + " ");
@@ -35,6 +37,7 @@ public class Sorts {
       System.out.println();
    }
 
+   // tests
    public static void main(String[] args) {
       int[] arr1 = {13, 6, 2, 0, 6, 21, -1};
       selectionSort(arr1);
